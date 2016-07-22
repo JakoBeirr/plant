d3d5ccee0356b20dc6f9d89e1972cfaed0adba0a
Jeroen Jansen <Jeroen Jansen>
2016-07-22 15:36:52 +0200

    delete added

--- src/main/java/be/boomkwekerij/plant/view/controller/CustomerListController.java
+++ src/main/java/be/boomkwekerij/plant/view/controller/CustomerListController.java
@@ -2,16 +2,22 @@ package be.boomkwekerij.plant.view.controller;
 
 import be.boomkwekerij.plant.controller.CustomerController;
 import be.boomkwekerij.plant.model.dto.CustomerDTO;
+import be.boomkwekerij.plant.util.CrudsResult;
 import be.boomkwekerij.plant.util.SearchResult;
 import be.boomkwekerij.plant.view.mapper.CustomerViewMapper;
 import be.boomkwekerij.plant.view.model.CustomerViewModel;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class CustomerListController implements Initializable {
@@ -38,9 +44,17 @@ public class CustomerListController implements Initializable {
     private TableColumn<CustomerViewModel, String> telephone;
     @FXML
     private TableColumn<CustomerViewModel, String> btwNumber;
+    @FXML
+    private Button deleteButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initData();
+
+        addChangeListenerToCustomerList();
+    }
+
+    private void initData() {
         ArrayList<CustomerViewModel> customers = new ArrayList<>();
 
         SearchResult<CustomerDTO> customerSearchResult = customerController.getAllCustomers();
@@ -52,5 +66,51 @@ public class CustomerListController implements Initializable {
         }
 
         customerList.getItems().setAll(customers);
+    }
+
+    private void addChangeListenerToCustomerList() {
+        customerList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<CustomerViewModel>() {
+            @Override
+            public void changed(ObservableValue<? extends CustomerViewModel> observable, CustomerViewModel oldValue, CustomerViewModel newValue) {
+                deleteButton.setVisible(newValue != null);
+            }
+        });
+    }
+
+    public void deleteCustomer(Event event) {
+        try {
+            CustomerViewModel selectedCustomer = customerList.getSelectionModel().getSelectedItem();
+            CrudsResult deleteResult = customerController.deleteCustomer(selectedCustomer.getId());
+
+            if (deleteResult.isSuccess()) {
+                handleDeleteSuccess();
+            } else {
+                handleDeleteError(deleteResult.getMessages());
+            }
+        } catch (Exception e) {
+            handleDeleteException(e);
+        }
+    }
+
+    private void handleDeleteSuccess() {
+        initData();
+        AlertController.alertSuccess("Klant verwijderd!");
+    }
+
+    private void handleDeleteError(List<String> errorMessages) {
+        StringBuilder errorBuilder = new StringBuilder("Gefaald wegens volgende fout(en): ");
+        for (int i = 0; i < errorMessages.size(); i++) {
+            String errorMessage = errorMessages.get(i);
+            errorBuilder.append(errorMessage);
+
+            if (i != (errorMessages.size()-1)) {
+                errorBuilder.append("; ");
+            }
+        }
+        AlertController.alertError("Klant verwijderen gefaald!", errorBuilder.toString());
+    }
+
+    private void handleDeleteException(Exception e) {
+        AlertController.alertException("Klant verwijderen gefaald!", e);
     }
 }
--- src/main/java/be/boomkwekerij/plant/view/controller/PlantListController.java
+++ src/main/java/be/boomkwekerij/plant/view/controller/PlantListController.java
@@ -2,16 +2,22 @@ package be.boomkwekerij.plant.view.controller;
 
 import be.boomkwekerij.plant.controller.PlantController;
 import be.boomkwekerij.plant.model.dto.PlantDTO;
+import be.boomkwekerij.plant.util.CrudsResult;
 import be.boomkwekerij.plant.util.SearchResult;
 import be.boomkwekerij.plant.view.mapper.PlantViewMapper;
 import be.boomkwekerij.plant.view.model.PlantViewModel;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PlantListController implements Initializable {
@@ -30,9 +36,17 @@ public class PlantListController implements Initializable {
     private TableColumn<PlantViewModel, String> measure;
     @FXML
     private TableColumn<PlantViewModel, Double> price;
+    @FXML
+    private Button deleteButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initData();
+
+        addChangeListenerToPlantList();
+    }
+
+    private void initData() {
         ArrayList<PlantViewModel> plants = new ArrayList<>();
 
         SearchResult<PlantDTO> plantSearchResult = plantController.getAllPlants();
@@ -44,5 +58,51 @@ public class PlantListController implements Initializable {
         }
 
         plantList.getItems().setAll(plants);
+    }
+
+    private void addChangeListenerToPlantList() {
+        plantList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<PlantViewModel>() {
+            @Override
+            public void changed(ObservableValue<? extends PlantViewModel> observable, PlantViewModel oldValue, PlantViewModel newValue) {
+                deleteButton.setVisible(newValue != null);
+            }
+        });
+    }
+
+    public void deletePlant(Event event) {
+        try {
+            PlantViewModel selectedPlant = plantList.getSelectionModel().getSelectedItem();
+            CrudsResult deleteResult = plantController.deletePlant(selectedPlant.getId());
+
+            if (deleteResult.isSuccess()) {
+                handleDeleteSuccess();
+            } else {
+                handleDeleteError(deleteResult.getMessages());
+            }
+        } catch (Exception e) {
+            handleDeleteException(e);
+        }
+    }
+
+    private void handleDeleteSuccess() {
+        initData();
+        AlertController.alertSuccess("Plant verwijderd!");
+    }
+
+    private void handleDeleteError(List<String> errorMessages) {
+        StringBuilder errorBuilder = new StringBuilder("Gefaald wegens volgende fout(en): ");
+        for (int i = 0; i < errorMessages.size(); i++) {
+            String errorMessage = errorMessages.get(i);
+            errorBuilder.append(errorMessage);
+
+            if (i != (errorMessages.size()-1)) {
+                errorBuilder.append("; ");
+            }
+        }
+        AlertController.alertError("Plant verwijderen gefaald!", errorBuilder.toString());
+    }
+
+    private void handleDeleteException(Exception e) {
+        AlertController.alertException("Plant verwijderen gefaald!", e);
     }
 }
